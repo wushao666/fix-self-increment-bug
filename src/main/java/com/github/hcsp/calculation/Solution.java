359811110dcd334d1afdce3fa30c64c9233d5729
Bo Zhang <bo@gradle.com>
2019-06-08 18:09:03 +0800

    Update repo

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -2,7 +2,7 @@ package com.github.hcsp.calculation;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(printNumberTo10(9));
+        printNumberTo10(9);
     }
 
     // 打印从number开始到10的数字
@@ -10,8 +10,11 @@ public class Solution {
     // 例如，number=9，则打印9,10
     // 现在输出的结果和预期不符，请修复此问题
     public static void printNumberTo10(int number) {
-        while (++number < 10) {
-            System.out.println(number);
+        while (true) {
+            System.out.println(++number);
+            if (number > 10) {
+                return;
+            }
         }
     }
 }
