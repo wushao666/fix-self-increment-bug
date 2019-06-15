a097c91a89b5ffd24f5ce8d492fcb932cb52de8b
Bo Zhang <bo@gradle.com>
2019-06-15 19:25:11 +0800

    Update repo

--- src/test/java/com/github/hcsp/calculation/SolutionTest.java
+++ src/test/java/com/github/hcsp/calculation/SolutionTest.java
@@ -1,13 +1,14 @@
-
 package com.github.hcsp.calculation;
-import org.junit.jupiter.api.Test;;import org.hamcrest.Matchers;;import com.github.blindpirate.extensions.CaptureSystemOutput;
+
+import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
-@Test
-@CaptureSystemOutput
-public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
-  capture.expect(Matchers.containsString(("(?s).*5\\s+6\\s+7\\s+8\\s+9\\s+10.*"))));
-  Solution.printNumberTo10(5);
-}
-
+    @Test
+    @CaptureSystemOutput
+    public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
+        capture.expect(Matchers.matchesRegex("(?s).*5\\s+6\\s+7\\s+8\\s+9\\s+10.*"));
+        Solution.printNumberTo10(5);
+    }
 }
