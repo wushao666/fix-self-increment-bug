6e4f9ba0f994b91a466c0a2562a1add154997145
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-07-01 15:54:25 +0800

    fix bug 2.0 (#235)
    fix bug 2.0 (#235)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -10,9 +10,10 @@ public class Solution {
     // 例如，number=9，则打印9,10
     // 现在输出的结果和预期不符，请修复此问题
     public static void printNumberTo10(int number) {
+        int temp = number;
         while (true) {
-            System.out.println(++number);
-            if (number > 10) {
+            System.out.println(temp++);
+            if (temp > 10) {
                 return;
             }
         }
